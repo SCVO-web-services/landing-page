c5fe286e115de8b54e9656f9680f6eaa0b7984a4
ImMamey <32584037+ImMamey@users.noreply.github.com>
2023-03-13 17:03:50 -0400

    [feat] Nested Dynamic Routes

--- next-routing/pages/product/[productId]/index.js
+++ next-routing/pages/product/[productId]/index.js
@@ -2,7 +2,7 @@ import {useRouter} from 'next/router'
 function ProductDetail()
 {
 	const router = useRouter()
-	const productId = router.query.productid
+	const productId = router.query.productId
     return <h1> Details about Product {productId}</h1>
 }
 export default ProductDetail
