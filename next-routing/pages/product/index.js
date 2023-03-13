d4de2d02d7d8f6e81215803376e0edbe45a9b792
ImMamey <32584037+ImMamey@users.noreply.github.com>
2023-03-13 18:44:22 -0400

    [feat] Elements to link routes

--- next-routing/pages/product/index.js
+++ next-routing/pages/product/index.js
@@ -1,10 +1,14 @@
-function ProductList()
+import Link from 'next/link'
+
+function ProductList({productId = 100})
 {
     return (
         <>
+        <Link href="/">Home</Link>
             <h2>Product 1</h2>
             <h2>Product 2</h2>
             <h2>Product 3</h2>
+            <h2><Link href={`/product/${productId}`}>Product {productId}</Link></h2>
         </>
     )
 }
