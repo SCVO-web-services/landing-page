d3ca2670c6f1fbafb56f6da1d82b98647bcb66be
ImMamey <32584037+ImMamey@users.noreply.github.com>
2023-03-13 21:12:07 -0400

    [feat] Fetching data, prerendering using components

--- next-pre-rendering/pages/users.js
+++ next-pre-rendering/pages/users.js
@@ -1,3 +1,5 @@
+import User from '../components/users'
+
 function UserList({users}) 
 {
     return <><h1>List of Users</h1>
@@ -5,8 +7,7 @@ function UserList({users})
         users.map((user) => {
             return (
                 <div key={user.id}>
-                    <p>{user.name}</p>
-                    <p>{user.email}</p>
+                    <User user={user}/>
                 </div>
             )
         })
