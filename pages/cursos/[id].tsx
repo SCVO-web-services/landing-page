75b90baa621c836d7bfdda3e20fdac7bc4fde837
ImMamey <32584037+ImMamey@users.noreply.github.com>
2025-01-11 17:30:08 -0400

    Lore Ipsum text
    .

--- pages/cursos/[id].tsx
+++ pages/cursos/[id].tsx
@@ -63,12 +63,12 @@ const CoursePage = ({ course }: CoursePageProps) => {
               <p className="font-bold text-black">Horario flexible</p>
             </div>
           </div>
-          <h1 className="text-3xl font-bold mb-4 text-black">{course.title}</h1>
+          <h1 className="text-3xl font-bold mb-4 text-black">Que aprenderás</h1>
           <ul className="list-disc pl-5 text-black">
-            <li>Lorem ipsum dolor sit amet</li>
-            <li>Consectetur adipiscing elit</li>
-            <li>Sed do eiusmod tempor incididunt</li>
-            <li>Ut labore et dolore magna aliqua</li>
+            <li>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</li>
+            <li>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</li>
+            <li>Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? </li>
+            <li>Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</li>
           </ul>
           <Button
             color="primary"
