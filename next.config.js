98080d1e9ab888e25c335674af56a17bd89320c4
ImMamey <colachambala@yahoo.es>
2025-03-18 12:22:04 -0400

    fix: removed BasePath

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: '/landing-page', // Add this line
   output: 'export',
   images: {
     unoptimized: true,
